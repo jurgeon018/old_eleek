cf619ed44274503081f0d24c7ec79f485d2fa81c
andrew mendela <jurgeon018@gmail.com>
2020-05-29 11:47:03 +0300

    s

--- project/__init__.py
+++ project/__init__.py
@@ -1,2 +1,12 @@
-default_app_config = 'project.apps.ProjectConfig'
 
+
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class ProjectConfig(AppConfig):
+    name = 'project'
+    verbose_name = _("Проект")
+    verbose_name_plural = verbose_name
+
+default_app_config = 'project.ProjectConfig'
--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include 
 
+from django.shortcuts import render 
+
+
+def index(request):
+    return render(request, 'project/index.html', locals())
+
+
 
 urlpatterns = [
-    # path('', i)
+    path('', index, name='index'),
+
 ]
