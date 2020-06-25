000fcc8d1f715b197c8c51794544ce74a1a11a99
andrew mendela <jurgeon018@gmail.com>
2020-06-25 21:33:43 +0000

    s

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,33 @@
-from django.urls import path, include 
-
 from django.shortcuts import render 
-
+from box.apps.sw_shop.sw_catalog.models import Item 
 
 def index(request):
     return render(request, 'project/index.html', locals())
 
+
+def about(request):
+    return render(request, 'project/about.html', locals())
+
+
+def items(request):
+    return render(request, 'project/items.html', locals())
+
+
+def item(request, slug):
+    return render(request, 'project/item.html', locals())
+
+
+def about(request):
+    return render(request, 'project/about.html', locals())
+
+
+def about(request):
+    return render(request, 'project/about.html', locals())
+
+
+
+
+from django.urls import path, include 
 
 
 urlpatterns = [
@@ -16,3 +38,11 @@ urlpatterns = [
     path('about/',       about, name='about'),
     path('about/',       about, name='about'),
 ]
+
+
+
+
+
+
+
+
