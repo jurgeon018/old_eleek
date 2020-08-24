b7417d91b92ccbe51199f2e7bc802e55de23625c
Lobatskiy Andriy <lobatskiy31@gmail.com>
2020-08-24 19:00:36 +0300

    debag_resize

--- static/development/components/interface/form/elements/list/index.js
+++ static/development/components/interface/form/elements/list/index.js
@@ -2,41 +2,41 @@ import "./index.scss";
 
 
 
-$(".form_box__item").on("click", function () {
-  $(this).toggleClass("form_box__item-active");
-  let item_input = $(this).find("input");
+// $(".form_box__item").on("click", function () {
+//   $(this).toggleClass("form_box__item-active");
+//   let item_input = $(this).find("input");
 
-  if (item_input.prop("checked") == true) {
-    item_input.prop("checked", false);
-  } else {
-    item_input.prop("checked", true);
-  }
+//   if (item_input.prop("checked") == true) {
+//     item_input.prop("checked", false);
+//   } else {
+//     item_input.prop("checked", true);
+//   }
 
-  let form_box__header = $(this).parents(".form_box").find(".form_box__header");
+//   let form_box__header = $(this).parents(".form_box").find(".form_box__header");
 
-  let box_item = $(this).parents(".form_box__main").find(".form_box__item");
+//   let box_item = $(this).parents(".form_box__main").find(".form_box__item");
 
-  if (box_item.length > 0) {
-    let count_item_active = 0;
+//   if (box_item.length > 0) {
+//     let count_item_active = 0;
 
-    for (const key in box_item) {
-      if (box_item.hasOwnProperty(key)) {
-        if (
-          $(box_item[key]).hasClass("form_box__item") &&
-          $(box_item[key]).hasClass("form_box__item-active")
-        ) {
-          count_item_active++;
-        }
-      }
-    }
+//     for (const key in box_item) {
+//       if (box_item.hasOwnProperty(key)) {
+//         if (
+//           $(box_item[key]).hasClass("form_box__item") &&
+//           $(box_item[key]).hasClass("form_box__item-active")
+//         ) {
+//           count_item_active++;
+//         }
+//       }
+//     }
 
-    if (count_item_active == box_item.length) {
-      form_box__header.addClass("form_box__header-active");
-    } else {
-      form_box__header.removeClass("form_box__header-active");
-    }
-  }
-});
+//     if (count_item_active == box_item.length) {
+//       form_box__header.addClass("form_box__header-active");
+//     } else {
+//       form_box__header.removeClass("form_box__header-active");
+//     }
+//   }
+// });
 
 $(".form_box__header").on("click", function () {
   let this_box = $(this);
--- static/source/pages/constructor_setings/index.js
+++ static/source/pages/constructor_setings/index.js
@@ -52412,38 +52412,36 @@ if (field_inputs.length > 0) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.scss */ "../components/interface/form/elements/list/index.scss");
 /* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_0__);
+ // $(".form_box__item").on("click", function () {
+//   $(this).toggleClass("form_box__item-active");
+//   let item_input = $(this).find("input");
+//   if (item_input.prop("checked") == true) {
+//     item_input.prop("checked", false);
+//   } else {
+//     item_input.prop("checked", true);
+//   }
+//   let form_box__header = $(this).parents(".form_box").find(".form_box__header");
+//   let box_item = $(this).parents(".form_box__main").find(".form_box__item");
+//   if (box_item.length > 0) {
+//     let count_item_active = 0;
+//     for (const key in box_item) {
+//       if (box_item.hasOwnProperty(key)) {
+//         if (
+//           $(box_item[key]).hasClass("form_box__item") &&
+//           $(box_item[key]).hasClass("form_box__item-active")
+//         ) {
+//           count_item_active++;
+//         }
+//       }
+//     }
+//     if (count_item_active == box_item.length) {
+//       form_box__header.addClass("form_box__header-active");
+//     } else {
+//       form_box__header.removeClass("form_box__header-active");
+//     }
+//   }
+// });
 
-$(".form_box__item").on("click", function () {
-  $(this).toggleClass("form_box__item-active");
-  var item_input = $(this).find("input");
-
-  if (item_input.prop("checked") == true) {
-    item_input.prop("checked", false);
-  } else {
-    item_input.prop("checked", true);
-  }
-
-  var form_box__header = $(this).parents(".form_box").find(".form_box__header");
-  var box_item = $(this).parents(".form_box__main").find(".form_box__item");
-
-  if (box_item.length > 0) {
-    var count_item_active = 0;
-
-    for (var key in box_item) {
-      if (box_item.hasOwnProperty(key)) {
-        if ($(box_item[key]).hasClass("form_box__item") && $(box_item[key]).hasClass("form_box__item-active")) {
-          count_item_active++;
-        }
-      }
-    }
-
-    if (count_item_active == box_item.length) {
-      form_box__header.addClass("form_box__header-active");
-    } else {
-      form_box__header.removeClass("form_box__header-active");
-    }
-  }
-});
 $(".form_box__header").on("click", function () {
   var this_box = $(this);
   $(this).toggleClass("form_box__header-active");
@@ -54125,11 +54123,35 @@ function onClickCheckboxOptions() {
   $(".form_box__item").on("click", function () {
     $(this).toggleClass("form_box__item-active");
     var item_input = $(this).find("input");
+    console.log(item_input);
+    console.log(item_input.prop("checked"));
+    console.log(item_input[0].checked);
 
     if (item_input.prop("checked") == true) {
       item_input.prop("checked", false);
     } else {
       item_input.prop("checked", true);
+    }
+
+    var form_box__header = $(this).parents(".form_box").find(".form_box__header");
+    var box_item = $(this).parents(".form_box__main").find(".form_box__item");
+
+    if (box_item.length > 0) {
+      var count_item_active = 0;
+
+      for (var key in box_item) {
+        if (box_item.hasOwnProperty(key)) {
+          if ($(box_item[key]).hasClass("form_box__item") && $(box_item[key]).hasClass("form_box__item-active")) {
+            count_item_active++;
+          }
+        }
+      }
+
+      if (count_item_active == box_item.length) {
+        form_box__header.addClass("form_box__header-active");
+      } else {
+        form_box__header.removeClass("form_box__header-active");
+      }
     }
 
     var string_params = $(".constructor_setings").serializeArray();
